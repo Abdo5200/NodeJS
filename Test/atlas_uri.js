cf2a2842da3de2a9e0ef55bc1b1e986485474b9a
Abdo5200 <abdelrahman.mamdouh2200@gmail.com>
2025-03-08 15:03:07 +0200

    some modification to app.js

--- Test/atlas_uri.js
+++ Test/atlas_uri.js
@@ -1,2 +1,2 @@
 module.exports = uri =
-  "mongodb+srv://abdelrahman_mamdouh:AmdRyzen32200g@cluster0.henws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://abdelrahmanmamdouh2200:AmdRyzen32200g@cluster0.49hhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
