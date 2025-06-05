4e4fb7c95c1ed6cc2c807e8ca45f158c488e7e5c
Abdo5200 <abdelrahman.mamdouh2200@gmail.com>
2025-06-05 03:54:55 +0300

    removed unused comments

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -27,18 +26,6 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
-// const mongoConnection = require("./util/database").mongoConnection;
-
-//?this for sequelize and mysql
-// const sequelize = require("./util/database");
-// const Product = require("./models/product");
-// const Cart = require("./models/cart");
-// const CartItem = require("./models/cart-item");
-// const Order = require("./models/order");
-// const OrderItem = require("./models/order-item");
-// const { FORCE } = require("sequelize/lib/index-hints");
-//?-----------------------------------------------------------
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
@@ -61,17 +48,7 @@ app.use(async (req, res, next) => {
     console.log(err);
   }
 });
-//!this is the promise based
-// app.use((req, res, next) => {
-//   User.findById("67b77d80dfea294bc9892f98")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
@@ -93,42 +70,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-// mongoConnection(() => {
-//   app.listen(3000);
-// });
-
-//!this was for mysql connection
-// Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-// User.hasMany(Product);
-
-// User.hasOne(Cart);
-// Cart.belongsTo(User);
-// Cart.belongsToMany(Product, { through: CartItem });
-// Product.belongsToMany(Cart, { through: CartItem });
-// Order.belongsTo(User);
-// User.hasMany(Order);
-// Order.belongsToMany(Product, { through: OrderItem });
-
-// sequelize
-//   // .sync({ force: true })
-//   .sync()
-//   .then((result) => {
-//     return User.findByPk(1);
-//   })
-//   .then((user) => {
-//     if (!user) {
-//       console.log("the user is ", User.findByPk(1));
-//       return User.create({ name: "Abdelrahman", email: "test123@test.com" });
-//     }
-//     return user;
-//   })
-//   .then((user) => {
-//     return user.createCart();
-//   })
-//   .then(() => {
-//     app.listen(3000);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
